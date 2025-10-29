f80ee15f3ce7139e2e4dc3eff91efbcd9b97e0e9
maxmcc33@gmail.com <maxmcc33@gmail.com>
2025-10-29 15:47:57 +0000

    finance_day

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,12 @@
+SELECT
+date_date
+, ROUND(COUNT(orders_id),2) AS nb_transactions
+, ROUND(SUM(revenue),2) AS total_revenue
+, ROUND(SUM(revenue)/COUNT(orders_id),2) AS average_basket
+, ROUND(SUM(purchase_cost),2) AS total_purchase_cost
+, ROUND(SUM(shipping_fee),2) AS total_shipping_fee
+, ROUND(SUM(logcost),2) AS total_log_cost
+, ROUND(SUM(quantity),2) AS total_quantity
+FROM {{ ref('int_orders_operational') }}
+GROUP BY date_date
+
